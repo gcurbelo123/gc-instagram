503b351e440349ece80c4391c898662103570f99
gcurbelo123 <gcurbelo@csumb.edu>
2019-03-04 15:53:09 -0800

    Insta

---
+++ parsgram/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  parsgram
+//
+//  Created by Gilbert Curbelo on 3/4/19.
+//  Copyright © 2019 gcapps. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
